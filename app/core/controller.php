6dda2168890900c35a8821585db29e01c8c54536
azizrah15 <azizrahmansyah15@gmail.com>
2024-03-10 19:44:05 +0700

    Penambahan isi controller

---
+++ app/core/controller.php
@@ -0,0 +1,5 @@
+<?php
+//parent dari Class controller utama
+class Controller {
+  
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
 if(!session_id()) session_start();
 require_once "../app/init.php";
+
+//inisialisasi class App /core/app.php
 $new = new App;
